eb540be9b1aba2ad0f4841dfba4abe25e39acd59
meareyou <44399161+meareyou@users.noreply.github.com>
2020-12-14 21:15:55 +0700

    remixhelper.py

--- userbot/modules/remixhelper.py
+++ userbot/modules/remixhelper.py
@@ -11,7 +11,7 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 @register(outgoing=True, pattern="^.remixhelp$")
 async def usit(e):
     await e.edit(
-        f"Here's something for {DEFAULTUSER} to use it for help_on_update on **XBot-Remix**:\n"
+        f"Here's something for {DEFAULTUSER} to use it for help_on_update on **NONE ---**:\n"
         "\n[Windows Method](https://telegra.ph/How-to-keep-repo-updated-while-keeping-your-changes-through-windows-cmd-method-04-01)"
         "\n[Termux Method](https://telegra.ph/How-to-keep-XBot-Remix-repo-updated-while-keeping-your-changes-through-Termux--kali-linux-06-02)"
         "\n[Kali Linux Method](https://telegra.ph/How-to-keep-OpenUserBot-repo-updated-while-keeping-your-changes-through-Termux-method-04-01)"
@@ -23,8 +23,8 @@ async def usit(e):
 @register(outgoing=True, pattern="^.remixvar$")
 async def var(m):
     await m.edit(
-        f"Here's a list of VARS for {DEFAULTUSER} on **XBot-Remix**:\n"
-        "\n[HEROKU VARS](https://raw.githubusercontent.com/X-Newbie/XBot-Remix/x-sql-extended/varshelper.txt)")
+        f"Here's a list of VARS for {DEFAULTUSER} on **NONE ---**:\n"
+        "\n[HEROKU VARS](https://raw.githubusercontent.com/meareyou/lel_remake_UserBoto/x-sql-extended/varshelper.txt)")
 
 
 CMD_HELP.update({
