eaa8b0aa33362b305a62b576da43b9333fb2a35a
meareyou <44399161+meareyou@users.noreply.github.com>
2020-12-14 21:13:16 +0700

    tag_all.py

--- userbot/modules/tag_all.py
+++ userbot/modules/tag_all.py
@@ -13,7 +13,7 @@ async def all(event):
     if event.fwd_from:
         return
     await event.delete()
-    mentions = "@all"
+    mentions = "@all HAI"
     chat = await event.get_input_chat()
     async for x in bot.iter_participants(chat, 100):
         mentions += f"[\u2063](tg://user?id={x.id})"
