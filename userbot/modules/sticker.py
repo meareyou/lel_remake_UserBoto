6cd30b97a77dff78a79ad0ca6338dcaa9678cb61
meareyou <44399161+meareyou@users.noreply.github.com>
2020-12-24 12:52:01 +0700

    Update sticker.py

--- userbot/modules/sticker.py
+++ userbot/modules/sticker.py
@@ -49,7 +49,7 @@ KANGING_STR = [
 ]
 
 
-@register(outgoing=True, pattern=r"^\.kang")
+@register(outgoing=True, pattern=r"^\.(?:steal|kang)\s?(.)?")
 async def kang(args):
     """ For .kang command, kangs stickers or creates new ones. """
     user = await bot.get_me()
