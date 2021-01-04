8892ead3cf0a15b7b4b7d93ed9a894c3ec5a80fe
meareyou <jeliokaleb@gmail.com>
2021-01-04 15:09:10 +0000

    pylint: auto fixes
    Signed-off-by: meareyou <jeliokaleb@gmail.com>

--- userbot/modules/tlkmsl.py
+++ userbot/modules/tlkmsl.py
@@ -1,9 +1,7 @@
-import os
-from time import sleep
-from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from userbot.events import register
-from userbot import bot, CMD_HELP
+from userbot import bot
+
 
 @register(outgoing=True, pattern=r"^\.tlkmsl(?: |$)(.*)")
 async def _(event):
@@ -14,7 +12,7 @@ async def _(event):
     await event.edit("`Processing...`")
     async with event.client.conversation(chat) as conv:
         try:
-            msg = await conv.send_message(now)
+            await conv.send_message(now)
             response = await conv.get_response()
             """ - don't spam notif - """
             await bot.send_read_acknowledge(conv.chat_id)
@@ -25,4 +23,4 @@ async def _(event):
             await event.edit("`Please cek bot and complete your identity`")
             return
         else:
-           await event.edit(f"{response.message.message}")
+            await event.edit(f"{response.message.message}")
