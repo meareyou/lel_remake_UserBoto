a2741e88bf338d6c43b4d047fbf90a7dc6e75324
lxs7599 <59435336+frrrrrits@users.noreply.github.com>
2021-01-05 09:26:41 +0700

    Update tlkmsl.py

--- userbot/modules/tlkmsl.py
+++ userbot/modules/tlkmsl.py
@@ -9,25 +9,26 @@ async def _(event):
         return
     chat = "@telkomsel_official_bot"
     now = f"cek kuota"
+    ya = f"Ya"
     await event.edit("`Processing...`")
-    async with event.client.conversation(chat) as conv:
+    async with bot.conversation(chat) as conv:
         try:
             response = conv.wait_event(
                 events.NewMessage(
                     incoming=True,
                     from_users=266446332))
             await bot.send_message(chat, now)
-            response = await response
-            """ - don't spam notif - """
-            response = await conv.get_response()
+            response = await response           
         except YouBlockedUserError:
             await event.reply("`Please unblock the Bot `@telkomsel_official_bot`")
             return
         if response.text.startswith("Agar"):
             await event.edit("`Please cek bot and complete your identity`")
-            return
-        else:
-            await event.edit(f"{response.message.message}")
+            return              
+           else:
+            await event.delete()
+            await bot.send_message(chat, ya) 
+            await bot.send_message(event.chat_id, response.message)
 
 CMD_HELP.update({
     "telkomsel":
