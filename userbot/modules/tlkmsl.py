290695a397182348587c87cb8578c1a40513ff34
meareyou <44399161+meareyou@users.noreply.github.com>
2021-01-07 13:26:35 +0700

    Update tlkmsl.py

--- userbot/modules/tlkmsl.py
+++ userbot/modules/tlkmsl.py
@@ -4,7 +4,7 @@ from telethon.errors.rpcerrorlist import YouBlockedUserError
 from userbot import bot
 from userbot.events import register
 
-
+#response 1
 @register(outgoing=True, pattern=r"^\.tlkmsl")
 async def _(event):
     if event.fwd_from:
@@ -22,6 +22,34 @@ async def _(event):
                     from_users=266446332))
             sleep(3)
             await bot.send_message(chat, agere)
+            response = await response
+            """ - don't spam notif - """
+            await bot.send_read_acknowledge(conv.chat_id)
+        except YouBlockedUserError:
+            await event.reply("unblock me (@telkomsel_official_bot) and try again")
+            return
+        if response.text.startswith("Untuk"):
+            await event.edit("Please cek bot and complete your identity")
+        else:
+            await event.delete()
+            await bot.send_read_acknowledge(conv.chat_id)
+            await event.client.send_message(event.chat_id, response.message)
+
+#response 2
+@register(outgoing=True, pattern="^\.tlkmls")
+async def _(event):
+    if event.fwd_from:
+        return
+    chat = "@telkomsel_official_bot"   
+    now = "cek kuota" 
+    await event.edit("Processing..")
+    async with event.client.conversation(chat) as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(
+                    incoming=True,
+                    from_users=266446332))
+            await bot.send_message(chat, now)          
             response = await response
             """ - don't spam notif - """
             await bot.send_read_acknowledge(conv.chat_id)
