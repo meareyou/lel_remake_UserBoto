a6477b44f404247c1ee9defa1d3da4c901fc83f3
lxs7599 <59435336+frrrrrits@users.noreply.github.com>
2021-01-07 12:45:48 +0700

    Create tlkmsl.py

---
+++ userbot/modules/tlkmsl.py
@@ -0,0 +1,31 @@
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from userbot import bot, CMD_HELP
+from userbot.events import register
+
+@register(outgoing=True, pattern="^\.tlkmsl")
+async def _(event):
+    if event.fwd_from:
+        return
+    chat = "@telkomsel_official_bot"   
+    now = "cek kuota" 
+    await event.edit("Processing..")
+    async with event.client.conversation(chat) as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(
+                    incoming=True,
+                    from_users=266446332))
+            await bot.send_message(chat, now)          
+            response = await response
+            """ - don't spam notif - """
+            await bot.send_read_acknowledge(conv.chat_id)
+        except YouBlockedUserError:
+            await event.reply("unblock me (@telkomsel_official_bot) and try again")
+            return
+        if response.text.startswith("Untuk"):
+            await event.edit("Please cek bot and complete your identity")
+        else:
+            await event.delete()
+            await bot.send_read_acknowledge(conv.chat_id)
+            await event.client.send_message(event.chat_id, response.message)
