a2c7e10939d7634d6441415424c3113cc87ac14c
meareyou <44399161+meareyou@users.noreply.github.com>
2021-01-16 13:20:15 +0700

    Update gban.py

--- userbot/modules/gban.py
+++ userbot/modules/gban.py
@@ -3,9 +3,11 @@ Group Administrations bots where you are SUDO
 Available Commands:
 .gban REASON
 .ungban REASON"""
+from telethon import events
 import asyncio
 from userbot.events import register
-from userbot import CMD_HELP, bot, G_BAN_LOGGER_GROUP, ALIVE_NAME
+from userbot import bot, CMD_HELP, G_BAN_LOGGER_GROUP, ALIVE_NAME
+
 # imported from uniborg by @heyworld
 
 # ================= CONSTANT =================
@@ -16,31 +18,28 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 @register(outgoing=True, pattern="^.gban(?: |$)(.*)")
 async def _(event):
     if G_BAN_LOGGER_GROUP is None:
-        await event.edit("Set G_BAN_LOGGER_GROUP in vars otherwise module won't work.")
+        await event.edit("ENV VAR is not set. This module will not work.")
         return
     if event.fwd_from:
         return
     reason = event.pattern_match.group(1)
     if event.reply_to_msg_id:
         r = await event.get_reply_message()
-        if r.forward:
-            r_from_id = r.forward.from_id or r.from_id
-        else:
-            r_from_id = r.from_id
+        r_from_id = r.forward.from_id or r.from_id if r.forward else r.from_id
         await bot.send_message(
             G_BAN_LOGGER_GROUP,
-            "/gban [user](tg://user?id={}) {}".format(r_from_id, reason)
+            "/gban [user](tg://user?id={}) {}".format(r_from_id, reason),
         )
-    await event.reply("**gbanning...**")
-    await event.edit(f"**User gbanned by {DEFAULTUSER}**")
-    asyncio.sleep(5)
     await event.delete()
+    await event.reply("`gbanning...`")
+    asyncio.sleep(3.5)
+    await event.reply(f"**User gbanned by** `{DEFAULTUSER} `")
 
 
 @register(outgoing=True, pattern="^.ungban(?: |$)(.*)")
 async def _(event):
     if G_BAN_LOGGER_GROUP is None:
-        await event.edit("Set G_BAN_LOGGER_GROUP in vars otherwise module won't work.")
+        await event.edit("ENV VAR is not set. This module will not work.")
         return
     if event.fwd_from:
         return
@@ -50,20 +49,21 @@ async def _(event):
         r_from_id = r.from_id
         await bot.send_message(
             G_BAN_LOGGER_GROUP,
-            "/ungban [user](tg://user?id={}) {}".format(r_from_id, reason)
+            "/ungban [user](tg://user?id={}) {}".format(r_from_id, reason),
         )
     await event.delete()
-    await event.reply("**ungbanning...**")
+    await event.reply("`ungbanning...`")
     asyncio.sleep(3.5)
-    await event.edit(f"**User ungbanned by {DEFAULTUSER}**")
-    asyncio.sleep(5)
-    await event.delete()
+    await event.reply(f"**User ungbanned by** `{DEFAULTUSER} `")
 
 
-CMD_HELP.update({
-    "gban": "\
+CMD_HELP.update(
+    {
+        "gban": "\
 `.gban reason`\
 \nUsage: Globally Ban users from all the Group Administrations bots where you are SUDO.\
 \n\n`.ungban reason`\
 \nUsage: Globally unBan users from all the Group Administrations bots where you are SUDO"
-})
+    }
+)
+#Credit OUBNEW
