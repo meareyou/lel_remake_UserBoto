98c1d2132919d3a57865b8ab34f23e59fa764299
lxs7499 <59435336+frrrrrits@users.noreply.github.com>
2021-01-17 12:10:43 +0700

    Add files via upload

---
+++ userbot/modules/aniairlings.py
@@ -0,0 +1,80 @@
+import datetime
+import json
+import asyncio
+import html
+import json
+import textwrap
+from io import BytesIO, StringIO
+from userbot import CMD_HELP
+from userbot.events import register
+
+#time formatter from uniborg
+def t(milliseconds: int) -> str:
+    """Inputs time in milliseconds, to get beautified time,
+    as string"""
+    seconds, milliseconds = divmod(int(milliseconds), 1000)
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    tmp = ((str(days) + " Days, ") if days else "") + \
+        ((str(hours) + " Hours, ") if hours else "") + \
+        ((str(minutes) + " Minutes, ") if minutes else "") + \
+        ((str(seconds) + " Seconds, ") if seconds else "") + \
+        ((str(milliseconds) + " ms, ") if milliseconds else "")
+    return tmp[:-2]
+    
+query = '''
+    query ($id: Int,$search: String) { 
+      Media (id: $id, type: ANIME,search: $search) { 
+        id
+        title {
+          romaji
+          native
+        }
+          episodes
+          chapters
+          volumes
+          season
+          type
+          format
+          status
+          duration
+          averageScore
+          genres
+          bannerImage
+        nextAiringEpisode {
+           airingAt
+           timeUntilAiring
+           episode
+        }
+      }
+    }
+    '''
+
+@register(outgoing=True, pattern=r"^.airlings ?(.*)")
+async def _(event):
+    q_ = event.pattern_match.group(1)
+    r_ = await event.get_reply_message()
+    if not q_:
+        await event.edit('Usage: airlings <anime name>')
+        event.delete()
+        return
+    url = "https://graphql.anilist.co"
+    vrb = {"search": q_}
+    res = requests.post(url,json={
+        "query": airing_query,
+        "variables": vrb
+    }).json()["data"]["media"]
+    
+    i_m = res.get("bannerImage",None)
+    t_r = res["title"]["romaji"]
+    t_n = res["title"]["native"]
+    i_d = res["id"]
+    msg = "*Name: *{t_r}*(`{t_n`})*ID: `{i_d}`[ ] ({image})*"  
+    await event.edit(msg)
+    
+CMD_HELP.update({
+    "airlings":
+        ".airlings <Anime name>\
+        \nUSAGE: Show anime airing in time"
+})
