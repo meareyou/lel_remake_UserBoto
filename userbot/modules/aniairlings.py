cef40286790f9a787144de1d467fb7ef2bde3aed
meareyou <jeliokaleb@gmail.com>
2021-01-17 09:07:44 +0000

    pylint: auto fixes
    Signed-off-by: meareyou <jeliokaleb@gmail.com>

--- userbot/modules/aniairlings.py
+++ userbot/modules/aniairlings.py
@@ -1,15 +1,11 @@
-import datetime
 import json
-import html
 import json
-import textwrap
 import requests
-import asyncio
 from userbot import CMD_HELP
 from userbot.events import register
 
 
-#time formatter from uniborg
+# time formatter from uniborg
 def t(milliseconds: int) -> str:
     """Inputs time in milliseconds, to get beautified time,
     as string"""
@@ -23,12 +19,12 @@ def t(milliseconds: int) -> str:
         ((str(seconds) + " Seconds, ") if seconds else "") + \
         ((str(milliseconds) + " ms, ") if milliseconds else "")
     return tmp[:-2]
- 
+
 
 def _api(str_):
     query = '''
-    query ($id: Int,$search: String) { 
-      Media (id: $id, type: ANIME,search: $search) { 
+    query ($id: Int,$search: String) {
+      Media (id: $id, type: ANIME,search: $search) {
         id
         title {
           romaji
@@ -58,17 +54,21 @@ def _api(str_):
     }
     '''
     variables = {
-        'search' : str_
+        'search': str_
     }
     url = 'https://graphql.anilist.co'
-    response = requests.post(url, json={'query': query, 'variables': variables})
+    response = requests.post(
+        url,
+        json={
+            'query': query,
+            'variables': variables})
     return response.text
-    
- 
+
+
 def jsonResult(resp):
     msg = ""
     mData = json.loads(resp)
-    err = list(mData.keys()) 
+    err = list(mData.keys())
     if "errors" in err:
         msg += f"**Anime** : `{mData['errors'][0]['message']}`"
         return msg
@@ -97,10 +97,10 @@ async def _(event):
         return
     mJson = _api(q_)
     mData = jsonResult(mJson)
-    await event.edit(mData,link_preview=True)
-    
+    await event.edit(mData, link_preview=True)
+
 CMD_HELP.update({
     "Anime Airing":
         "Usage: .airling <Anime Name>\
         \nShows you the airing of the anime"
-    })
+})
