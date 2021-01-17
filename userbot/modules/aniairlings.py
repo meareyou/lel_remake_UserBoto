68d23d213cdc0321259776f32e0643a2e29601e6
meareyou <44399161+meareyou@users.noreply.github.com>
2021-01-17 19:03:00 +0700

    Update aniairlings.py

--- userbot/modules/aniairlings.py
+++ userbot/modules/aniairlings.py
@@ -81,7 +81,7 @@ def jsonResult(resp):
     else:
         mResult = mData['data']['Media']
         image = mResult['bannerImage']
-        msg += f"**Name** : **{mResult['title']-['romaji']}**(`{mResult['title']['native']}`)\n**ID**: '{mResult['id']}`[⁠ ⁠]({image})""
+        msg += f"**Name** : **{mResult['title']-['romaji']}**(`{mResult['title']['native']}`)\n**ID** : '{mResult['id']}`[⁠ ⁠]({image})"
         if mResult['nextAiringEpisode']:
             time = mResult['nextAiringEpisode']['timeUntilAiring'] * 1000
             time = t(time)
