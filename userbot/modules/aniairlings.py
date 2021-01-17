2c009a84aef76feb7de93cbe5279c2d5e2f43801
lxs7499 <59435336+frrrrrits@users.noreply.github.com>
2021-01-18 00:10:07 +0700

    Update aniairlings.py
    hedeuh mpss dah

--- userbot/modules/aniairlings.py
+++ userbot/modules/aniairlings.py
@@ -90,8 +90,8 @@ async def _(event):
         time = time_(time)
         msg += f"\n**Episode**: `{data['nextAiringEpisode']['episode']}`"
         msg += f"\n**Airing in**: `{time}`"
-        await event.client.send_file(event.chat_id, file=coverImg, caption=msg, reply_to=event)
-        # return msg
+        #await event.client.send_file(event.chat_id, file=coverImg, caption=msg, reply_to=event)
+        return msg
     else:
         msg += f"\n**Episode**: `{episodes}`"
         msg += f"\n**Status**: `N/A`"
