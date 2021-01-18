60387b94afc65068a1e2393a459025f174af83a4
meareyou <jeliokaleb@gmail.com>
2021-01-18 08:49:10 +0000

    pylint: auto fixes
    Signed-off-by: meareyou <jeliokaleb@gmail.com>

--- userbot/modules/neonime.py
+++ userbot/modules/neonime.py
@@ -1,18 +1,16 @@
-import html
 import requests
-import asyncio
-import html
 from bs4 import BeautifulSoup as bs
 import re
 from userbot import CMD_HELP
 from userbot.events import register
 
+
 @register(outgoing=True, pattern=r"^\.neonime ?(.*)")
 async def _(event):
     url = 'https://neonime.vip/episode/'
     ht_ = requests.get(url).text
-    _bs = bs(ht_,"html.parser")
-    bd_ = _bs.findAll('td',class_='bb')
+    _bs = bs(ht_, "html.parser")
+    bd_ = _bs.findAll('td', class_='bb')
     out = f"**New Episode**:\n"
     for kntl_ in bd_:
         _lucu = kntl_.find('a')
@@ -20,10 +18,10 @@ async def _(event):
             _lucu = 'none'
         else:
             tt_ = _lucu.get_text()
-            _tt = re.sub(r'\s+Subtitle\s+Indonesia\s+Season.\d+','',tt_)
+            _tt = re.sub(r'\s+Subtitle\s+Indonesia\s+Season.\d+', '', tt_)
             link = hh['href']
             out += f"- <a href='{link}'>{_tt}</a>"
-            await event.edit(out,parse_mode='html')
+            await event.edit(out, parse_mode='html')
             await event.delete()
 
 CMD_HELP.update({
