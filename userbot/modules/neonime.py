69137ba7c0efc05bb93da25dfdf40a56d63736e0
lxs7499 <59435336+frrrrrits@users.noreply.github.com>
2021-01-18 19:53:34 +0700

    Update neonime.py
    hedueh

--- userbot/modules/neonime.py
+++ userbot/modules/neonime.py
@@ -21,7 +21,7 @@ async def _neonime(event):
             tt_ = _lucu.get_text()
             _tt = re.sub(r'\s+Subtitle\s+Indonesia\s+Season.\d+', '', tt_)
             link = _lucu['href']
-            out += f"∗ั- <a href='{link}'>{_tt}</a>\n"
+            out += f""▪ <a href='{link}'>{_tt}</a>\n"
             if len(out) > 1000:
                 break
             await event.edit(out, parse_mode="html")
@@ -40,9 +40,10 @@ async def _neolink(event):
         req = requests.get(uri).text
         sho = bs(req, 'html.parser')
         eps = sho.findAll('div', class_='sbox')
-        res = f"<b>{eps}</b>"
-        await event.edit(res, parse_mode='html')
-
+        for res in eps:
+            st = f'{res}'
+            tt = re.sub(r'</a>','\n',st)
+            await event.edit(tt,parse_mode='html') 
 
 CMD_HELP.update({
     "neonime":
