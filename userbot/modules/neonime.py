7098ed80ec43fecc4be1350ec27add8a1a5a1059
lxs7499 <59435336+frrrrrits@users.noreply.github.com>
2021-01-18 19:56:06 +0700

    Update neonime.py
    double donk

--- userbot/modules/neonime.py
+++ userbot/modules/neonime.py
@@ -21,7 +21,7 @@ async def _neonime(event):
             tt_ = _lucu.get_text()
             _tt = re.sub(r'\s+Subtitle\s+Indonesia\s+Season.\d+', '', tt_)
             link = _lucu['href']
-            out += f""▪ < a href = '{link}' > {_tt} < /a >\n"
+            out += f"▪ < a href = '{link}' > {_tt} < /a >\n"
             if len(out) > 1000:
                 break
             await event.edit(out, parse_mode="html")
