e7567491c4a4dab17d61a580a7676274cc83a40d
lxs7499 <59435336+frrrrrits@users.noreply.github.com>
2021-01-18 17:05:13 +0700

    Update neonime.py
    test cuk

--- userbot/modules/neonime.py
+++ userbot/modules/neonime.py
@@ -1,12 +1,14 @@
+import html
 import requests
+import asyncio
+import html
 from bs4 import BeautifulSoup as bs
 import re
 from userbot import CMD_HELP
 from userbot.events import register
 
-
 @register(outgoing=True, pattern=r"^\.neonime ?(.*)")
-async def _(event):
+async def _neonime(event):
     await event.edit('tunggu bentar...')
     url = 'https://neonime.vip/episode/'
     ht_ = requests.get(url).text
@@ -21,13 +23,32 @@ async def _(event):
             tt_ = _lucu.get_text()
             _tt = re.sub(r'\s+Subtitle\s+Indonesia\s+Season.\d+', '', tt_)
             link = _lucu['href']
-            out += f"- <a href='{link}'>{_tt}</a>\n"
+            out += f"∗ั- <a href='{link}'>{_tt}</a>\n"
             if len(out) > 1000:
                 break
             await event.edit(out, parse_mode="html")
 
+
+@register(outgoing=True, pattern=r"^\.neolink ?(.*)")
+async def _neolink(event):
+    uri = event.pattern_match.group(1)
+    if not uri:
+        await event.edit("Masukan url episode, liat .help neonime")
+    elif 'https://' not in uri:
+        await event.edit('Masukan url')
+        return
+    else:
+        req = requests.get(uri).text
+        sho = bs(req,'html.parser')
+        eps = sho.findAll('div',class_='sbox')
+        await event.edit(eps,parse_mode='html')
+        
+        
 CMD_HELP.update({
     "neonime":
         ".neonime\
-        \nUsage: liat anime baru rilis dari neonime"
+        \nUsage: liat anime baru rilis dari neonime\
+        \n.neolink <Url link>\
+        \nUsage: cari download link\
+        \n\nCopy Url link dari .neonime"
 })
