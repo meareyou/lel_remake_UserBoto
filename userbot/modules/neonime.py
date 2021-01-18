1940644a9eadbca018d1913b7308e42f7da860e4
lxs7499 <59435336+frrrrrits@users.noreply.github.com>
2021-01-18 16:10:53 +0700

    Update neonime.py
    ga ngeliat itu harusnya <b> tpi malah <n>

--- userbot/modules/neonime.py
+++ userbot/modules/neonime.py
@@ -12,7 +12,7 @@ async def _(event):
     ht_ = requests.get(url).text
     _bs = bs(ht_, "html.parser")
     bd_ = _bs.findAll('td', class_='bb')
-    out = "<b>New Episode:</n>\n"
+    out = "<b>New Episode:</b>\n\n"
     for kntl_ in bd_:
         _lucu = kntl_.find('a')
         if not _lucu:
