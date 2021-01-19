bfc452c83c5c9bbfaaa0a807069ea96fa3c58eff
meareyou <jeliokaleb@gmail.com>
2021-01-19 14:58:29 +0000

    pylint: auto fixes
    Signed-off-by: meareyou <jeliokaleb@gmail.com>

--- userbot/modules/kucingpeduli.py
+++ userbot/modules/kucingpeduli.py
@@ -7,7 +7,9 @@ from userbot import CMD_HELP
 from userbot.events import register
 
 r = Session()
-ua = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36'}
+ua = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36'}
+
 
 @register(outgoing=True, pattern=r"^\.neko ?(.*)")
 async def _search(event):
@@ -17,8 +19,8 @@ async def _search(event):
         return
     else:
         url = f"https://nekopoi.care/?s={qu}"
-        _a = r.get(url,headers=ua).text
-        _b = bs(_a,'html.parser')
+        _a = r.get(url, headers=ua).text
+        _b = bs(_a, 'html.parser')
         o_ = f"**Hasil search Dari kucing peduli**\n\n"
        if _b.find('div',attrs={'class':'postsbody'}).find('h2').text == "Tidak ada hasil":
           await event.edit('tidak ada hasil')
