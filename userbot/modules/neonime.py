13cc0499843b698a14ef777c98183ec35dbfdec7
meareyou <44399161+meareyou@users.noreply.github.com>
2021-01-20 16:07:49 +0700

    Update neonime.py

--- userbot/modules/neonime.py
+++ userbot/modules/neonime.py
@@ -31,6 +31,6 @@ async def _neonime(event):
 CMD_HELP.update({
     "neonime":
         "Download anime Dari Neonime\
-        \n\n.neonime\
-        \nUsage: liat anime baru rilis dari neonime\"
+        \n\n.neonime\"
+        "\nUsage: liat anime baru rilis dari neonime\"
 })
