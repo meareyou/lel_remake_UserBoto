4d85d2a50ca21b5142b47c78478e7c322b7f08d6
meareyou <44399161+meareyou@users.noreply.github.com>
2021-01-25 22:23:57 +0700

    Update String-Termux.sh

--- String-Termux.sh
+++ String-Termux.sh
@@ -5,20 +5,18 @@ green='\033[32;1m'
 red='\033[31;1m'
 yellow='\033[33;1m'
 HOME='/data/data/com.termux/files/home'
-clear
-neofetch
-printf '\n\n\n$green$time\n\n\n'
-printf $blue "\n1. Get String File for User\n2. Get String File for Bot$green\n3. Install Kebutuhan$yellow\n4. Clear File$red\n00. Cancel "
-read -p "\n????????? : " okl
-
+printf "\n\n\n$green$time\n\n\n"
+printf "\n1. Get String File for User\n2. Get String File for Bot\n3. Install Kebutuhan\n4. Clear File$red\n00. Cancel\n\n\n"
+read -p "????????? : " okl
+$yellow
 if [ $okl = 1 ]
 then
 cd $HOME
 python -V
 if [ ! -e string_session.py ]; then
-    printf  '\nDownloading string_session.py\n';wget https://raw.githubusercontent.com/meareyou/lel_remake_UserBoto/x-sql-extended/string_session.py;printf '\nRunning script...\n';sleep 1;python3 string_session.py
+    printf  "\nDownloading string_session.py\n";wget https://raw.githubusercontent.com/meareyou/lel_remake_UserBoto/x-sql-extended/string_session.py;printf "\nRunning script...\n";sleep 1;python3 string_session.py
 else
-    printf  '\nstring_session.py detected... \nrunning existing file\n';sleep 1;python3 string_session.py
+    printf  "\nstring_session.py detected... \nrunning existing file\n";sleep 1;python3 string_session.py
 fi
 fi
 
@@ -26,9 +24,9 @@ if [ $okl = 2 ]
 then
 cd $HOME;python -V
 if [ ! -e string_support_bot.py ]; then
-    printf  '\nDownloading string_session.py\n';wget https://raw.githubusercontent.com/meareyou/lel_remake_UserBoto/x-sql-extended/string_support_bot.py;printf '\nRunning script...\n';sleep 1;python3 string_support_bot.py
+    printf  "\nDownloading string_session.py\n";wget https://raw.githubusercontent.com/meareyou/lel_remake_UserBoto/x-sql-extended/string_support_bot.py;printf "\nRunning script...\n";sleep 1;python3 string_support_bot.py
 else
-    printf  '\nstring_support_bot.py detected... \nrunning existing file\n';sleep 1;python3 string_support_bot.py
+    printf  "\nstring_support_bot.py detected... \nrunning existing file\n";sleep 1;python3 string_support_bot.py
 fi
 fi
 
@@ -40,7 +38,7 @@ fi
 
 if [ $okl = 4 ]
 then
-printf 'Clearing not used files';sleep 2;cd $HOME;rm -rf requirements.txt;rm -rf string_session.py;rm -rf string_support_bot.py;clear;logout
+printf "Clearing not used files";sleep 2;cd $HOME;rm -rf requirements.txt;rm -rf string_session.py;rm -rf string_support_bot.py;clear;logout
 fi
 
 if [ $okl = 0 ] || [ $okl = 00 ]
