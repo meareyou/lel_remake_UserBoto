599e3733f191ded043e7c9463f7ba7edfbea902a
meareyou <44399161+meareyou@users.noreply.github.com>
2021-01-26 07:29:20 +0700

    Update String-Termux.sh
    Mmk

--- String-Termux.sh
+++ String-Termux.sh
@@ -1,17 +1,15 @@
-#!/system/xbin/Bash
+#!/system/xbin/bash
 time=$(date)
 blue='\033[34;1m'
 green='\033[32;1m'
 red='\033[31;1m'
-yellow='\033[33;1m'
-HOME='/data/data/com.termux/files/home'
-printf "\n\n\n$green$time\n\n\n"
-printf "\n1. Get String File for User\n2. Get String File for Bot\n3. Install Kebutuhan\n4. Clear File$red\n00. Cancel\n\n\n"
-read -p "????????? : " okl
-$yellow
-if [ $okl = 1 ]
+printf "%s\n\n\n$green$time\n\n\n"
+printf "%s\n1. Get String File for User\n2. Get String File for Bot\n3. Install Kebutuhan\n4. Clear File$red\n00. Cancel\n\n\n"
+read -r "????????? :" okl
+$blue
+if [ "$okl" = 1 ] ||  [ "$okl" = 01 ]
 then
-cd $HOME
+cd /data/data/com.termux/files/home || exit || exit
 python -V
 if [ ! -e string_session.py ]; then
     printf  "\nDownloading string_session.py\n";wget https://raw.githubusercontent.com/meareyou/lel_remake_UserBoto/x-sql-extended/string_session.py;printf "\nRunning script...\n";sleep 1;python3 string_session.py
@@ -20,28 +18,30 @@ else
 fi
 fi
 
-if [ $okl = 2 ]
+if [ "$okl" = 2 ] ||  [ "$okl" = 02 ]
 then
-cd $HOME;python -V
+cd /data/data/com.termux/files/home || exit;python -V
 if [ ! -e string_support_bot.py ]; then
     printf  "\nDownloading string_session.py\n";wget https://raw.githubusercontent.com/meareyou/lel_remake_UserBoto/x-sql-extended/string_support_bot.py;printf "\nRunning script...\n";sleep 1;python3 string_support_bot.py
 else
     printf  "\nstring_support_bot.py detected... \nrunning existing file\n";sleep 1;python3 string_support_bot.py
 fi
+sleep 90
 fi
 
-if [ $okl = 3 ]
+if [ "$okl" = 3 ] ||  [ "$okl" = 03 ]
 then
-pkg install python -y;pkg install wget -y;cd $HOME;wget https://raw.githubusercontent.com/meareyou/ezrequ/main/requirements.txt;pip install -r requirements.txt
+pkg install python -y;pkg install wget -y;cd /data/data/com.termux/files/home || exit;wget https://raw.githubusercontent.com/meareyou/ezrequ/main/requirements.txt;pip install -r requirements.txt
 clear
 fi
 
-if [ $okl = 4 ]
+if [ "$okl" = 4 ] ||  [ "$okl" = 04 ]
 then
-printf "Clearing not used files";sleep 2;cd $HOME;rm -rf requirements.txt;rm -rf string_session.py;rm -rf string_support_bot.py;clear;logout
+printf "Clearing not used files";sleep 2;cd /data/data/com.termux/files/home || exit;rm -rf requirements.txt;rm -rf string_session.py;rm -rf string_support_bot.py;clear;logout
 fi
 
-if [ $okl = 0 ] || [ $okl = 00 ]
+if [ "$okl" = 0 ] || [ "$okl" = 00 ]
 then
-cd $HOME;clear;logout
+cd /data/data/com.termux/files/home || exit;clear;logout
 fi
+clear
