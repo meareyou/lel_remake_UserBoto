5103f558988516181ded25c8862d5212bf1d745d
lxs7499 <59435336+frrrrrits@users.noreply.github.com>
2021-01-27 15:27:12 +0700

    Update kucingPeduli.py

--- userbot/modules/kucingPeduli.py
+++ userbot/modules/kucingPeduli.py
@@ -1,38 +1,40 @@
 from bs4 import BeautifulSoup as bs
-import requests as re
+import requests
 from userbot.events import register
 from userbot import CMD_HELP
 
-
 @register(outgoing=True, pattern=r"^.nekocare ?(.*)")
 async def _(event):
-    query = event.pattern_match.group(1)
-    if not query:
-        await event.edit("Usage: .nekocare <Hentai Name>")
-        return
-    att = []
-    a = f"https://nekopoi.care/?s={query}"
-    b = re.get(a).text
-    await event.edit(f"{b}\nPlease wait..")
-    c = bs(b, "html.parser")
-    if c.find('div', attrs={'class': 'postsbody'}).find(
-            'h2').text == "Tidak ada hasil":
-        await event.edit("not found")
-    else:
-        r = f"Result:\n"
-        d = c.find('div', class_="result").parent.find_all('li')
-        for arr in d:
-            a = arr.find('h2').text.strip()
-            b = arr.find('h2').findNext('a').attrs['href']
-            att.append({
-                "title": a,
-                "url": b,
-            })
-        for k, v in enumerate(att):
-            t = v["title"]
-            u = v["url"]
-            r += f"<a href='{u}'>{t}</a>"
-            await event.edit(r)
+    try:
+        query = event.pattern_match.group(1)
+        if not query:
+            await event.edit("Usage: .nekocare <Hentai Name>")
+            return
+        att = []
+        a = f"https://nekopoi.care/?s={query}&post_type=anime"
+        b = requests.get(a).text
+        await event.edit(f"{b}\nPlease wait..")
+        c = bs(b, "html.parser")
+        if c.find('div', attrs={'class': 'postsbody'}).find('h2').text == "Tidak ada hasil":
+            await event.edit("not found")
+        else:
+            r = f"Result:\n"
+            d = c.find('div', class_="result").parent.find_all('li')
+            for arr in d:
+                a = arr.find('h2').text.strip()
+                b = arr.find('h2').findNext('a').attrs['href']
+                att.append({
+                    "title": a,
+                    "url": b,
+                })
+            for k, v in enumerate(att):
+                t = v["title"]
+                u = v["url"]
+                r += f"<a href='{u}'>{t}</a>"
+                await event.delete()
+                await event.edit(r)
+    except:
+        return None
 
 CMD_HELP.update({
     "kucingPeduli":
