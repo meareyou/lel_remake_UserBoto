873a7d75711cf44b4ac9572a2eb1b758ef77ea69
meareyou <jeliokaleb@gmail.com>
2021-02-02 07:46:40 +0000

    pylint: auto fixes
    Signed-off-by: meareyou <jeliokaleb@gmail.com>

--- userbot/modules/nekocare.py
+++ userbot/modules/nekocare.py
@@ -71,7 +71,7 @@ def get_search_result(query):
         for src in search_body:
             title = src["title"]
             ids = src["id"]
-            message += f"• {title} \ [`{ids}`]\n"
+            message += rf"• {title} \ [`{ids}`]\n"
         return message
 
 
