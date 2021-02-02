5c2419cd5ef336554ee189aa37e30760b5e95d0b
lxs7499 <59435336+frrrrrits@users.noreply.github.com>
2021-02-02 14:37:02 +0700

    Update nekocare.py

--- userbot/modules/nekocare.py
+++ userbot/modules/nekocare.py
@@ -3,9 +3,6 @@ from userbot.events import register
 import requests
 from bs4 import BeautifulSoup as bs
 
-str_url = "http://nekopoi-index.herokuapp.com/"
-
-
 @register(outgoing=True, pattern=r"^\.kucing ?(.*)")
 async def _(event):
     query = event.pattern_match.group(1)
@@ -30,32 +27,30 @@ async def _(event):
 
 def search(anime):
     result_anime = []
-    search_url = requests.get(str_url + "?search=" + anime)
-    soup_html = bs(search_url.text, "html5lib")
-    soup_body = soup_html.find_all(
-        "tr",
-        class_="border-t border-b border-gray-300 my-2 p-4 rounded hover:bg-blue-100")
+    str_url = "http://nekopoi-index.herokuapp.com/"
+    search_url = requests.get(str_url+"?search="+anime)
+    soup_html = bs(search_url.text,"html5lib")
+    soup_body = soup_html.find_all("tr",class_="border-t border-b border-gray-300 my-2 p-4 rounded hover:bg-blue-100")
     if soup_body:
         for soup_result in soup_body:
-            result_id = soup_result.find(
-                "th", class_="my-2 p-2").get_text(strip=True)
-            result_bool = True if soup_result.find(
-                "th", string="True") else False
-            if result_bool:
-                result_title = soup_result.find(
-                    "a").text.strip().split("mp4", 40)
-                string_title = "".join(result_title) + ".."
-                result_anime.append({"title": string_title, "id": result_id})
+            result_id = soup_result.find("th",class_="my-2 p-2").get_text(strip=True)
+            result_bool = True if soup_result.find("th",string="True") else False
+            if result_bool == True:
+               result_title = soup_result.find("a").get_text()
+               string_title = re.sub(r"[\W\^_-]"," ",result_title)
+               my_title1 = re.sub(r"\s+"," ",string_title)
+               my_title2 = my_title1.replace(r"480P","")
+               my_title3 = my_title2.replace(r"mp4","")
+               result_anime.append({"title":my_title3.replace(r"NekoPoi",""),"id":result_id})
         return{
-            "result": result_anime,
-            "response": "200"
+            "result":result_anime,
+            "response":"200"
         }
     else:
         return{
-            "result": "Not Found",
-            "response": "404"
+            "result":"Not Found",
+            "response":"404"
         }
-
 
 def get_search_result(query):
     search_res = search(query)
@@ -68,19 +63,19 @@ def get_search_result(query):
         for src in search_body:
             title = src["title"]
             ids = src["id"]
-            message += f"   **>** `{title}` / **Id**:`{ids}`\n"
+            message += f"**Title**:{title}\n **Id**:`{ids}`\n"
         return message
-
-
+        
 @register(outgoing=True, pattern=r"^\.searchpoi ?(.*)")
 async def nekopoi(bot):
     query = bot.pattern_match.group(1)
     if not query:
         await bot.edit("Usage: <query>")
     else:
+        await bot.edit("Searching...")
         result = get_search_result(query)
         await bot.edit(result)
-
+        
 
 CMD_HELP.update({
     "nekocare":
