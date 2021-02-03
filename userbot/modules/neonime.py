767a8cad5e325bdb263e44caa9677488bdc14d41
lxs7499 <59435336+frrrrrits@users.noreply.github.com>
2021-02-03 15:36:03 +0700

    Update neonime.py
    p

--- userbot/modules/neonime.py
+++ userbot/modules/neonime.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup as bs
 import re
+import time
 from userbot import CMD_HELP
 from userbot.events import register
 
@@ -27,6 +28,57 @@ async def _neonime(event):
             if len(out) > 1000:
                 break
             await event.edit(out, parse_mode="html")
+
+def get_html(url):
+    tag_li = []
+    req = requests.get(url)
+    res = bs(req.text,"html5lib")
+    box = res.find("div",class_="sbox").parent.find_all("li")
+    if len(box) != 0:
+       for clear in box:
+           if clear.get_text() == 'MP4':
+               box.remove(clear)
+           elif clear.get_text() == 'MKV':
+               box.remove(clear)
+           else:pass
+    for box_ in box:
+        tag_li.append(box_)
+    return {
+        "html":tag_li
+    }
+    
+def link_download(query,url):
+    tag_label = []
+    tag_href = []
+    r = get_html(url)["html"]
+    for k,v in enumerate(r[query].find_all("a")):
+        tag_href.append({"server":v.get_text(strip=True),"link":v["href"]})
+    for p,o in enumerate(r[query].find_all("label")):
+        tag_label.append(o.get_text())
+    return {
+        "label":tag_label,
+        "url":tag_href
+    }
+  
+@register(outgoing=True, pattern=r"^\.neolink ?(.*)")
+async def _(event):
+    #tag_count = []    
+    url = event.pattern_match.group(1)
+    if not uri:
+        await event.edit("Masukan url episode, liat .help neonime")
+    elif 'https://' not in url:
+        await event.edit('Masukan url')
+        return
+    else:
+        msg = "Link Download\n\n"
+        p = link_download(1,url)
+        for label_name in p["label"]:
+            msg += f"- {label_name}:\n"
+        for server_link in p["url"]:
+            server_name = server_link["server"]
+            server_url = server_link["link"]
+            msg += f"<a href='{server_name}'>{server_url}</a>｜"
+       await event.edit(msg,parse_mode="html")
 
 CMD_HELP.update({
     "neonime":
